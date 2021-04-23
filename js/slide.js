620937105cc0f3547464d4d2c7b51a36317ba6b9
iraholdshtein <iragold80@ukr.net>
2021-04-23 19:43:49 +0300

    Added second slider, Scroll.

--- js/slide.js
+++ js/slide.js
@@ -5,5 +5,23 @@ $(document).ready(function(){
         dots: false,
         slidesToShow: 3,
         slidesToScroll: 3, 
+        
     });
   });
+
+  
+
+  
+  $(function() {
+  $(window).scroll(function() {
+  if($(this).scrollTop() != 0) {
+  $('#topNubex').fadeIn();
+  } else {
+  $('#topNubex').fadeOut();
+  }
+  });
+  $('#topNubex').click(function() {
+  $('#Topscroll,html').animate({scrollTop:0},10);
+  });
+  });
+ 
