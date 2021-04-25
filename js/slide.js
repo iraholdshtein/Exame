8f57506502091fd8434e802078317449fa074d03
iraholdshtein <iragold80@ukr.net>
2021-04-26 00:19:30 +0300

    burger-menu and  added slider-arrows section5

--- js/slide.js
+++ js/slide.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
               }
             },
             {
-              breakpoint: 480,
+              breakpoint: 320,
               settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1
@@ -51,7 +51,10 @@ $(document).ready(function(){
 
             
 $('.header-burger').click(function(event){
-    $('.header-burger','.header-menu').toggleClass('active');
+    $('body').toggleClass('lock');
+});
+
+$('.header-link').click(function(event){
     $('body').toggleClass('lock');
 });
                     
