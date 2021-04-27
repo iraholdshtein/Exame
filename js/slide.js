a46d42c36d9424a71227a5c768609ab52c51f1e0
iraholdshtein <iragold80@ukr.net>
2021-04-28 02:16:55 +0300

    adaptive layout

--- js/slide.js
+++ js/slide.js
@@ -4,32 +4,79 @@ $(document).ready(function(){
         arrows:true,
         dots: false,
         slidesToShow: 3,
-        slidesToScroll: 3, 
+        slidesToScroll: 3,
+        adaptiveheihts: true,
+        adaptivewidth: true,
         responsive: [
+          {
+            breakpoint: 1600,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              arrows:true,
+             
+            }
+          },
+            
             {
-              breakpoint: 1024,
+              breakpoint: 1200,
               settings: {
                 slidesToShow: 3,
                 slidesToScroll: 3,
-                infinite: true,
-                dots: true
+                arrows:true,
+
               }
             },
             {
-              breakpoint: 600,
+                breakpoint: 992,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: false,
+                  arrows:true,
+  
+                }
+              },
+              {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow:2,
+                  slidesToScroll: 2,
+                  infinite: true,
+                  dots: false,
+                  arrows:true,
+  
+                }
+              },
+            {
+              breakpoint: 576,
               settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+                infinite: true,
+                arrows:true
               }
             },
             {
               breakpoint: 320,
               settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                dots:false,
+                infinite:true,
+                arrows:false,
+                adaptiveheihts:true,
+                autoplay: true,
+                auytoSpeed:300,
+                PauseOnHover: true,
+                PauseOnDots: true,
+                PauseOnDotsHover: true
               }
             }
           ]
+        
     });
   });
 
